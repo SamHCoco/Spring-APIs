96c87a9c89662deb6dc6289591ca3ba236a0c6b3
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-21 06:57:41 +0000

    Get request for all TV Series in the database implemented.

--- TV-Series-API/src/main/java/tvseries/api/controller/TVSeriesAPIController.java
+++ TV-Series-API/src/main/java/tvseries/api/controller/TVSeriesAPIController.java
@@ -1,14 +1,22 @@
 package tvseries.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tvseries.api.TVSeries;
+import tvseries.api.TVSeriesService;
+
+import java.util.List;
 
 @RestController
 public class TVSeriesAPIController {
 
-    @RequestMapping("/tv-series")
-    public String displayTVSeries(){
-        return "TEST RESPONSE: Here are your TV series";
+    @Autowired
+    TVSeriesService tvSeriesService;
+
+    @GetMapping("/tv-series")
+    public List<TVSeries> getAllTVSeries(){
+        return tvSeriesService.getAllTVSeries();
     }
 
 }
