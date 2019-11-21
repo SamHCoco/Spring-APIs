ed546cb8a544cc29e87298396edeb841f73f3ce2
Samuel Coco <42847052+SamHCoco@users.noreply.github.com>
2019-11-21 06:23:44 +0000

    Application Main class

---
+++ TV-Series-API/src/main/java/tvseries/api/TVSeriesAPIApplication.java
@@ -0,0 +1,12 @@
+package tvseries.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TVSeriesAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TVSeriesAPIApplication.class, args);
+	}
+}
