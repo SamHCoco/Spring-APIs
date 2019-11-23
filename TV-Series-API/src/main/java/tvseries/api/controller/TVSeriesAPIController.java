0d3c9afc0a287b1fe190fefcd5115c69cb059400
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-23 16:44:12 +0000

    addTVSeries() and deleteTVSeries() mapped

--- TV-Series-API/src/main/java/tvseries/api/controller/TVSeriesAPIController.java
+++ TV-Series-API/src/main/java/tvseries/api/controller/TVSeriesAPIController.java
@@ -1,6 +1,8 @@
 package tvseries.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import tvseries.api.TVSeries;
@@ -12,22 +14,52 @@ import java.util.List;
 public class TVSeriesAPIController {
 
     @Autowired
-    TVSeriesService tvSeriesService;
+    private TVSeriesService tvSeriesService;
 
+    /**
+     * Handles GET requests for all TV series in the database, or all TV series
+     * filtered by optional "genre" or release "year" query parameters.
+     * @param genre The desired genre of TV series.
+     * @param year The desired release year for TV series.
+     * @return ResponseEntity with HTTP status code 200 and response body containing queried
+     * TV series if resource found, or HTTP code 404 response if not.
+     */
     @GetMapping("/tv-series")
-    public List<TVSeries> getAllTVSeries(){
-        return tvSeriesService.getAllTVSeries();
+    public ResponseEntity<List<TVSeries>> getAllTVSeries(@RequestParam(required = false) String genre,
+                                                         @RequestParam(required = false) Integer year){
+        List<TVSeries> tvSeries = tvSeriesService.getAllTVSeries(genre, year);
+        if(tvSeries.size() > 0){
+            return new ResponseEntity<>(tvSeries, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    // todo - Add HTTP 404 Response if TV Series resource not found
     @GetMapping("/tv-series/{id}")
-    public TVSeries getTVSeries(@PathVariable int id){
-        return tvSeriesService.getTVSeries(id);
+    public ResponseEntity<TVSeries> getTVSeries(@PathVariable int id){
+        TVSeries tvShow = tvSeriesService.getTVSeries(id);
+        if(tvShow != null){
+            return new ResponseEntity<>(tvShow, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/tv-series")
-    public void addTVSeries(@RequestBody TVSeries tvSeries){
+    public ResponseEntity addTVSeries(@RequestBody TVSeries tvSeries){
         tvSeriesService.addTVSeries(tvSeries);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tv-series/{id}")
+    public ResponseEntity deleteTVSeries(@PathVariable int id){
+        boolean isDeleteSuccessful;
+        isDeleteSuccessful = tvSeriesService.deleteTVSeries(id);
+        if(isDeleteSuccessful){
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
