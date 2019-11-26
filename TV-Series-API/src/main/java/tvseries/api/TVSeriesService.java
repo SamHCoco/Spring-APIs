34443dcbece0bfa69c00208ec9304562cbffc8d4
Sam Coco <Samuel.HenriAC@outlook.com>
2019-11-26 19:29:43 +0000

    Documenation added.

--- TV-Series-API/src/main/java/tvseries/api/TVSeriesService.java
+++ TV-Series-API/src/main/java/tvseries/api/TVSeriesService.java
@@ -12,6 +12,12 @@ public class TVSeriesService {
     @Autowired
     private TVSeriesRepository tvSeriesRepository;
 
+    /**
+     * Service to get all TV series in database, or just a subset, filtered by release year or genre, or both.
+     * @param genre The desired genre (Adventure, Sci-Fi, Comedy-Drama or Crime).
+     * @param year The desired release year for TV series.
+     * @return All TV series in the database, or all series filtered by year, genre or both.
+     */
     public List<TVSeries> getAllTVSeries(String genre, Integer year){
         List<TVSeries> tvSeries = new ArrayList<>();
         if(genre == null && year == null){
@@ -29,6 +35,11 @@ public class TVSeriesService {
         return tvSeries;
     }
 
+    /**
+     * Service to get a single TV series from the database, specified by its ID.
+     * @param id The ID of the desired TV series.
+     * @return The queried TV series, or null if it's not present in the database.
+     */
     public TVSeries getTVSeries(int id){
         if(tvSeriesRepository.findById(id).isPresent()){
             return tvSeriesRepository.findById(id).get();
@@ -36,6 +47,11 @@ public class TVSeriesService {
         return null;
     }
 
+    /**
+     * Service to add a single TV series to the database.
+     * @param tvSeries The TV series to be added to the database.
+     * @return True if the TV series as successfully added, or false otherwise.
+     */
     public boolean addTVSeries(TVSeries tvSeries){
         boolean isAdded = false;
         tvSeriesRepository.save(tvSeries);
@@ -45,6 +61,11 @@ public class TVSeriesService {
         return isAdded;
     }
 
+    /**
+     * Service to delete a single TV series from the database.
+     * @param id The ID of the TV series to be deleted.
+     * @return True if the TV series was successfully deleted, or false otherwise.
+     */
     public boolean deleteTVSeries(int id){
         boolean isDeleted = false;
         if(tvSeriesRepository.findById(id).isPresent()){
