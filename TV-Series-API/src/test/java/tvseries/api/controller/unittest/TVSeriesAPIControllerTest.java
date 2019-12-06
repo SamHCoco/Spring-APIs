76034b9f7246b7ad5865ed22262c3c13da24442a
Sam Coco <Samuel.HenriAC@outlook.com>
2019-12-06 18:11:19 +0000

    java.lang.AssertionError on line 94 to be fixed

--- TV-Series-API/src/test/java/tvseries/api/controller/unittest/TVSeriesAPIControllerTest.java
+++ TV-Series-API/src/test/java/tvseries/api/controller/unittest/TVSeriesAPIControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,8 +19,11 @@ import tvseries.api.controller.TVSeriesAPIController;
 
 import java.util.List;
 
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +36,7 @@ public class TVSeriesAPIControllerTest {
     @MockBean
     private TVSeriesService tvSeriesService;
 
+    @Autowired
     private ObjectMapper objectMapper;
 
     private List<TVSeries> tvSeries;
@@ -52,6 +57,51 @@ public class TVSeriesAPIControllerTest {
 
         System.out.println("\n*************************RESPONSE BODY**************************************");
         System.out.println(result.getResponse().getContentAsString() + "\n");
+    }
+
+    @Test
+    public void getTVSeriesTest() throws Exception {
+        TVSeries expected = null;
+        for(TVSeries tvShow : tvSeries){
+            if(tvShow.getId() == 9){
+                expected = tvShow; // Expected TV Series = Attack on Titan (It has ID 9 in test database)
+            }
+        }
+        if(expected != null){
+            when(tvSeriesService.getTVSeries(9)).thenReturn(expected);
+
+            MvcResult result = mockMvc.perform(get("/tv-series/9")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk()).andReturn();
+
+            String expectedJSON = objectMapper.writeValueAsString(expected);
+            String controllerResponseJSON = result.getResponse().getContentAsString();
+            JSONAssert.assertEquals(controllerResponseJSON, expectedJSON, true);
+        } else {
+            fail();
+        }
+    }
+
+    @Test
+    public void addTVSeriesTest() throws Exception {
+        TVSeries testSeries = new TVSeries(1, "title", 0, "genre", "description");
+        when(tvSeriesService.addTVSeries(testSeries)).thenReturn(true);
+
+        MvcResult result = mockMvc.perform(post("/tv-series")
+                .content(objectMapper.writeValueAsBytes(testSeries))
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8"))
+                .andExpect(status().isCreated()) // todo - fix java.lang.AssertionError on line 94
+                .andReturn();
+    }
+
+    @Test
+    public void deleteTVSeries() throws Exception {
+        when(tvSeriesService.deleteTVSeries(1)).thenReturn(true);
+
+        MvcResult result = mockMvc.perform(delete("/tv-series")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
     }
 
 }
