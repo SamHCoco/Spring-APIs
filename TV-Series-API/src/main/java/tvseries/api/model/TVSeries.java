88e95990a9b946e5f184fed955982f6cd0cc956d
Sam Coco <Samuel.HenriAC@outlook.com>
2019-12-09 21:59:30 +0000

    Model edited

---
+++ TV-Series-API/src/main/java/tvseries/api/model/TVSeries.java
@@ -0,0 +1,73 @@
+package tvseries.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TVSeries {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private int year;
+    private String genre;
+    private String description;
+
+    public TVSeries(){
+
+    }
+
+    public TVSeries(int id, String title, int year, String genre, String description) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.description = description;
+    }
+
+
+    // SETTERS
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //  GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
