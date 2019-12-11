5a3c3d7b3a2a6fdf424ceb2e041eec4628a9af44
Sam Coco <Samuel.HenriAC@outlook.com>
2019-12-11 14:16:14 +0000

    JpaRepository base package class annotation added

--- TV-Series-API/src/main/java/tvseries/api/TVSeriesAPIApplication.java
+++ TV-Series-API/src/main/java/tvseries/api/TVSeriesAPIApplication.java
@@ -2,8 +2,12 @@ package tvseries.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import tvseries.api.repository.TVSeriesRepository;
+import tvseries.api.security.repository.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = {UserRepository.class, TVSeriesRepository.class})
 public class TVSeriesAPIApplication {
 
 	public static void main(String[] args) {
