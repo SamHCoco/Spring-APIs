f6d8c1557b482898c623f2ad9459d6401c5958a5
Sam Coco <Samuel.HenriAC@outlook.com>
2019-12-30 07:23:03 +0000

    main method for running application

---
+++ Aircraft-API/src/main/java/com/SamHCoco/aircraft/api/AircraftRestAPIApplication.java
@@ -0,0 +1,13 @@
+package com.SamHCoco.aircraft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftRestAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftRestAPIApplication.class, args);
+	}
+
+}
